1882765c54581b8439d5752d4903e979e85941f0
cristian <bogalox@gmail.com>
2021-12-14 13:53:01 +0100

    ok

--- Web/autocert_chrome.py
+++ Web/autocert_chrome.py
@@ -16,28 +16,25 @@ from datetime import datetime as dt
 
 
 date = dt.now()
-day = date.day
+day = date.day+7
 month = date.month
 year = date.year
 
 gform_faculty_menu = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[1]/div[1]/span'
-gform_faculty = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div[8]'
+gform_faculty = "//*[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div[8]/span"
 gform_name = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input'
 gform_surname = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input'
 gform_building_menu = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div[1]/div[1]/div[1]/span'
 gform_building = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div[2]/div[90]/span'
 gform_hour_menu = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div[1]/div[1]/div[1]'
 gform_hour = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div[2]/div[6]/span'
-gform_month = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/input'
-gform_day = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/input'
-gform_year = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/input'
 gform_consent = '/html/body/div[1]/div[2]/form/div[2]/div/div[2]/div[9]/div/div/div[2]/div[1]/div/label/div/div[1]/div[2]'
 gform_enter = '/html/body/div[1]/div[2]/form/div[2]/div/div[3]/div[1]/div[1]/div[2]/span/span'
 
 option = webdriver.ChromeOptions()
 #option.add_argument("-incognito")
-#option.add_argument("--headless")
-#option.add_argument("disable-gpu")
+# option.add_argument("--headless")
+# option.add_argument("disable-gpu")
 option.add_argument("user-data-dir=/home/cristian/.config/google-chrome/Default");
 browser = webdriver.Chrome(executable_path='/home/cristian/PhD/autocertificazioni/chromedriver', options=option)
 browser.get("https://docs.google.com/forms/d/e/1FAIpQLSdAG_KegcP-AIyDqb875yaFw3dd9xn8k65zDTOXsjtIoLTFKQ/viewform")
@@ -52,6 +49,13 @@ gform_faculty_menu_element = browser.find_element('xpath', gform_faculty_menu)
 browser.execute_script("arguments[0].click();", gform_faculty_menu_element)
 WebDriverWait(browser, 20).until(EC.element_to_be_clickable(('xpath', gform_faculty))).click()
 
+
+
+
+secondtext[2].send_keys(month)
+secondtext[2].send_keys(day)
+secondtext[2].send_keys(year)
+
 gform_hour_menu_element = browser.find_element('xpath', gform_hour_menu)
 browser.execute_script("arguments[0].click();", gform_hour_menu_element)
 WebDriverWait(browser, 20).until(EC.element_to_be_clickable(('xpath', gform_hour))).click()
@@ -60,11 +64,10 @@ gform_building_menu_element = browser.find_element('xpath', gform_building_menu)
 browser.execute_script("arguments[0].click();", gform_building_menu_element)
 WebDriverWait(browser, 20).until(EC.element_to_be_clickable(('xpath', gform_building))).click()
 
-secondtext[2].send_keys(month)
-secondtext[2].send_keys(day)
-secondtext[2].send_keys(year)
+
 
 gform_consent_element = browser.find_element('xpath', gform_consent)
 browser.execute_script("arguments[0].click();", gform_consent_element)
+
 submit=browser.find_element(By.XPATH,"//*[@id='mG61Hd']/div[2]/div/div[3]/div[1]/div[1]/div[2]")
 submit.click()
