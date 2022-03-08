e11bea67a5e9c47ef59d0bdbc86def5b5b695d80
sp-ino <spinovale97@gmail.com>
2022-03-08 23:03:48 +0100

    create fourier

---
+++ Plots/improved/fourier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Tue Mar  8 22:58:26 2022
+
+@author: spino
+Copyright (c) 2022 Valerio Spinogatti
+Licensed under GNU license
+"""
